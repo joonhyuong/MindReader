6ec83e59fd98151e6e72d4ade69aa86077298fa0
joonhyuong <nacjeebar@naver.com>
2023-05-21 18:21:38 +0900

    앱 구동 시 권한 메시지 출력

--- app/src/main/java/com/example/calendertest/MainActivity.kt
+++ app/src/main/java/com/example/calendertest/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.calendertest
 
+import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import android.provider.MediaStore
 import android.view.View
 import android.widget.Button
@@ -13,6 +16,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.example.calendertest.databinding.ActivityImgBinding
 import com.example.calendertest.databinding.ActivityMainBinding
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var diaryTextView: TextView
     lateinit var imgBtn: Button
     lateinit var textBtn: Button
+    private val REQUEST_PERMISSIONS=1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         diaryTextView=findViewById(R.id.diaryTextView)
         imgBtn=findViewById(R.id.imgBtn)
         textBtn=findViewById(R.id.textBtn)
-
+        checkPermission()
         calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
             diaryTextView.visibility = View.VISIBLE
             imgBtn.visibility = View.VISIBLE
@@ -43,22 +48,49 @@ class MainActivity : AppCompatActivity() {
         imgBtn.setOnClickListener({
             val intent = Intent(this, ImgActivity::class.java)
             startActivity(intent)
-        })
 
+        })
+    }
+    private fun checkPermission() {
+        var permission = mutableMapOf<String, String>()
+        permission["camera"] = Manifest.permission.CAMERA
+        permission["storageRead"] = Manifest.permission.READ_EXTERNAL_STORAGE
+        permission["storageWrite"] =  Manifest.permission.WRITE_EXTERNAL_STORAGE
+
+        // 현재 권한 상태 검사
+        var denied = permission.count { ContextCompat.checkSelfPermission(this, it.value)  == PackageManager.PERMISSION_DENIED }
+
+        // 마시멜로 버전 이후
+        if(denied > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permission.values.toTypedArray(), REQUEST_PERMISSIONS)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == REQUEST_PERMISSIONS) {
+            /*권한 요청을 거부했다면 안내 메시지 보여주며 앱 종료*/
+            grantResults.forEach {
+                if(it == PackageManager.PERMISSION_DENIED) {
+                    Toast.makeText(applicationContext, "서비스의 필요한 권한입니다.\n권한에 동의해주세요.", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
     }
 
 }
 
 class ImgActivity : AppCompatActivity() {
     private lateinit var binding: ActivityImgBinding
-    //lateinit var galleryBtn:Button
-    //lateinit var imageView: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImgBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setContentView(R.layout.activity_img)
-        //galleryBtn=findViewById(R.id.galleryBtn)
 
 //버튼 이벤트
         binding.galleryBtn.setOnClickListener {
@@ -92,7 +124,6 @@ class ImgActivity : AppCompatActivity() {
 class CameraActivity : AppCompatActivity() {
     lateinit var bitmap: Bitmap
     lateinit var imageView: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
@@ -110,6 +141,7 @@ class CameraActivity : AppCompatActivity() {
         }
     }//onCreate
 
+
     //결과 가져오기
     private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()){
@@ -125,4 +157,5 @@ class CameraActivity : AppCompatActivity() {
             imageView.setImageBitmap(bitmap)
         }
     }
+
 }
